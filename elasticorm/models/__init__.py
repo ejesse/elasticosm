85ac20eac860c70f745a46a938b60962ef8ea314
Jesse Emery <jesse@jesseemery.com>
2012-06-01 09:58:05 -0700

    Fix not properly doing deletes for git

--- elasticorm/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from elasticorm.models.base import BaseElasticModel
-from elasticorm.models.fields import BooleanField, DateTimeField, \
-    FloatField, IntField, LongField, ReferenceField, StringField, SearchField, AutoDateTimeField, CreationDateTimeField, ReferenceField
-import datetime
-
-class ElasticModel(BaseElasticModel):
-    
-    created_date = CreationDateTimeField()
-    last_modified_date = AutoDateTimeField()
